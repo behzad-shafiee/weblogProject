a0c596c7b9dec2ceced67e062c638915af17f909
behzadShafiee <behzadshafieejoon1999@yahoo.com>
2022-03-26 09:51:45 -0700

    hw20-p8

--- src/validators/validatores.js
+++ src/validators/validatores.js
@@ -113,8 +113,8 @@ module.exports = new class {
     arrOfvalidateRegister() {
 
         return [
-            body('userName').notEmpty().withMessage("userName is empty").isLength({ min: 2, max: 30 }).withMessage('username must be at least 2 charcator'),
-            body('password').notEmpty().withMessage("password is empty").isLength({ min: 8, max: 15 }).withMessage('password must be at least 8 charcator'),
+            body('userName').notEmpty().withMessage("userName is empty").isLength({ min: 2, max: 30 }).withMessage('username must be between 2-30'),
+            body('password').notEmpty().withMessage("password is empty").isLength({ min: 8, max: 15 }).withMessage('password must be between 8-15'),
             body('firstName').notEmpty().withMessage("firstName is empty").isLength({ min: 2, max: 30 }).withMessage('firstname must be between 2-30'),
             body('lastName').notEmpty().withMessage("lastName is empty").isLength({ min: 2, max: 30 }).withMessage('lastname must be between 2-30'),
             body('phoneNumber').notEmpty().withMessage("phoneNumber is empty")
@@ -123,57 +123,43 @@ module.exports = new class {
     }
 
 
-    doValidateRegister(req, res, next) {
+    async doValidateRegister(req, res, next) {
+        try {
 
-        console.log(req.body);
-
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
+            const errors = validationResult(req);
             console.log(errors);
-            return res.render('registerPage', { error: 'something is wrong!!!' });
+            if (!errors.isEmpty()) {
+
+                return res.render('registerPage', { error: 'all fields must fill and userName,firstName,lastName must be between 2-30 and password must be between 8-15' });
+            }
+
+            const phoneNumber = req.body.phoneNumber;
+            if (!validator.isMobilePhone(phoneNumber, ['fa-IR'])) {
+
+                return res.render('registerPage', { error: 'phoneNumber must be 11 digit and start with 09..' });
+
+            };
+
+            const regExp = /^(?=[^A-Z\n]*[A-Z])(?=[^a-z\n]*[a-z])(?=[^0-9\n]*[0-9])(?=[^#?!@$%^&*\n-]*[#?!@$%^&*-]).{8,}$/g;
+            const checkPass = regExp.test(req.body.password);
+            if (!checkPass) {
+
+                return res.render('registerPage', { error: 'password must be atleast one capital letter one small letter one number one special letter' });
+            };
+
+            const isExist = await Blogger.findOne({ userName: req.body.userName });
+            if (isExist) {
+
+                return res.render('registerPage', { error: 'userName must be uniqe' });
+
+            }
+            next();
+
+        } catch (err) {
+
+            console.log(`err of doValidateRegister:${err}`);
         }
-
-        const phoneNumber = req.body.phoneNumber;
-        if (!validator.isMobilePhone(phoneNumber, ['fa-IR'])) {
-
-            console.log(`phoneNumber must be 11 digit and start with 09..`);
-            return res.render('registerPage', { error: 'something is wrong!!!' });
-
-        }
-
-
-
-
-        const regExp = /^(?=[^A-Z\n]*[A-Z])(?=[^a-z\n]*[a-z])(?=[^0-9\n]*[0-9])(?=[^#?!@$%^&*\n-]*[#?!@$%^&*-]).{8,}$/g;
-        const checkPass = regExp.test(req.body.password);
-        if (!checkPass) {
-            console.log(req.body.password);
-            console.log('password must be atleast one capital letter one small letter one number one special letter');
-            return res.render('registerPage', { error: 'something is wrong!!!' });
-        };
-
-
-
-        Blogger.findOne({ userName: req.body.username })
-            .then((checkIsExistUser) => {
-
-                if (checkIsExistUser) {
-
-                    return res.render('registerPage', { error: 'something is wrong!!!' });
-
-                }
-                next();
-            })
-            .catch(err => {
-
-                console.log(`err of finding blogger in doValidateRegister:${err}`);
-            })
-
-
-
-
 
 
     }
-
 }
